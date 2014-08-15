71580df7808841c722977ad9527bf271cda798a2
Shane R. Spencer <shane@bogomip.com>
2014-08-15 19:27:52 +0000

    fixup how pages are found and used

--- tornado_template_server/__init__.py
+++ tornado_template_server/__init__.py
@@ -3,6 +3,6 @@ __author__ = 'Shane R. Spencer'
 __author_email__ = "shane@bogomip.com"
 __license__ = 'MIT'
 __copyright__ = '2014 Shane R. Spencer'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __status__ = "Prototype"
 __description__ = "Tornado Template Server for dynamically serving template data (includes data-uri uimodule)"
